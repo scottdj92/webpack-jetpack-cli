6ce698ccad8725387a8c3ac4edd1c3073b110fbb
Scott Jones <scottdj92@gmail.com>
2016-08-23 10:42:26 -0400

    Add testing items, and made writing to file work correctly

---
+++ configs/base.js
@@ -0,0 +1,22 @@
+var config = {
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+    },
+    plugins: [
+
+    ],
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.css', '.js', '.jsx', '.scss']
+    },
+    module: {
+        loaders: {
+
+        }
+    }
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 #!/usr/bin/env node
-
 'use strict';
 
 const program = require('commander'),
     inquirer = require('inquirer'),
     chalk = require('chalk'),
-    pkg = require('./package.json');
+    pkg = require('./package.json'),
+    fs = require('fs');
 
 let createConfig = (options) => {
     console.log('Fueling jetpack...');
+    start();
+    //openFile();
+};
+
+let start = () => {
+    fs.open(__dirname + '/export/webpack.config.js', "w+", function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("opened file in:");
+
+        var config = require('./configs/base');
+        console.log(config);
+        writeFile(config);
+    });
+};
+
+let writeFile = (data) => {
+    console.log("Building configuration");
+    fs.writeFile(__dirname + '/export/webpack.config.js', data.config, function(err) {
+        if (err) {
+            return console.error("error writing file", err);
+        }
+    });
 };
 
 program
     .version(pkg.version)
     .command('fuel')
-    .description('Fueling jetpack...')
+    .description('bootstraps a basic webpack configuration file')
     .action(createConfig);
 program.parse(process.argv);
