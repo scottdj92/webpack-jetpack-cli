1192ad0c3d3bfea5d98eef555c97e9b6037cff94
Scott Jones <scottdj92@gmail.com>
2016-08-22 23:08:03 -0400

    Initial scaffold

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+
+program
+    .version('')
+    .command('')
+    .description('')
+    .option('')
+    .option('')
+    .action('');
+program.parse(process.argv);
